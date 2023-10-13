71f8a55d81aa598dc8c866d265502edfbb7709bd
kazz <gang9696@gmail.com>
2023-10-13 18:07:09 +0900

    초기 커밋.

---
+++ WinMain.cpp
@@ -0,0 +1,110 @@
+#include <windows.h>
+#include "D2DFramework.h"
+
+const wchar_t gClassName[] = L"MyWindowClass";
+
+#define SAFE_RELEASE(p) { if(p) {p->Release(); p=0;}}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR lpCmdLine,
+    _In_ int nShowCmd)
+{
+    // D2D Factory / single - 조금 느리지만 데드락이 없어 안전
+    // &gpD2DFactory 반환할 값. 포인터 포인터 형식
+   
+    D2DFramework  myFramework;
+
+    HWND hwnd;
+    WNDCLASSEX wc;
+    ZeroMemory(&wc, sizeof(WNDCLASSEX));
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpszClassName = gClassName;
+    wc.hInstance = hInstance;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.lpfnWndProc = WindowProc;
+    wc.cbSize = sizeof(WNDCLASSEX);
+    if (!RegisterClassEx(&wc))
+    {
+        MessageBox(nullptr, L"Failed to register window class!", L"Error", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    RECT wr = { 0, 0, 1024, 768 };
+    AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
+    hwnd = CreateWindowEx(NULL,
+        gClassName,
+        L"Direct2D",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        wr.right - wr.left,
+        wr.bottom - wr.top,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
+    if (hwnd == nullptr)
+    {
+        MessageBox(nullptr, L"Failed to create window class!", L"Error", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+    
+    myFramework.Initialize(hwnd);
+
+    ShowWindow(hwnd, nShowCmd);
+    UpdateWindow(hwnd);
+
+    /*MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        OnPaint(hwnd);
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    */
+
+    MSG msg;
+    while (true)
+    {
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+
+            if (msg.message == WM_QUIT)
+            {
+                break;
+            }
+        }
+        else
+        {
+            myFramework.Render();
+        }
+    }
+    myFramework.Release();
+
+    return static_cast<int>(msg.wParam);
+
+}
+
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    
+    case WM_CLOSE:
+        DestroyWindow(hwnd);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, message, wParam, lParam);
+    }
+    return 0;
+}
